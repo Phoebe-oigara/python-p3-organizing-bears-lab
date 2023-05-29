9c88c354238fef5d1f7d39d2b971748685be4e93
Phoebe Oigara <phoebeoigara@gmail.com>
2023-05-29 19:11:28 +0300

    Basic SQL operations hacked

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES
+    (' Mr. Chocolate', 13, 'M', 'brown' ,'lazy', 1),
+     ('Rowdy', 15,'M', 'brown', 'loud',1),
+    ('Tabitha', 20,'F', 'black', 'calm',1),
+    ('Sergeant Brown', 30,'M' ,'green' ,'deadly',1),
+    ('Melissa', 18, 'F', 'grey' ,'sweet', 1),
+    ('Grinch', 50,'M', 'green', 'grumpy', 1),
+    ('Wendy', 16 , 'F', 'white', 'cheeky', 1),
+    (NULL, 35, 'F', 'blue' , NULL, 1);
+
